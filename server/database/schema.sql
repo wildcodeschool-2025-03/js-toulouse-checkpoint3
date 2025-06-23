0cda5ddbf77b7d66a1d0da50d93d5c7eae8f385f
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-06-23 10:22:52 +0200

    finished step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE tile (
     id INT AUTO_INCREMENT NOT NULL,
     type VARCHAR(255) NOT NULL,
     coord_x INT NOT NULL,
-    coord_y INT NOT NULL,
+    coord_y INT NOT NULL,    
+    has_treasure BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
