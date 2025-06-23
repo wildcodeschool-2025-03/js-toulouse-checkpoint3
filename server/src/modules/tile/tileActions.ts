c5146ba4e7c436809b70c91dac4731ca0e921cf4
zarzour <ridouane@MacBook-Air-de-ridouane.local>
2025-06-23 13:30:40 +0200

    etape3

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,9 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  const tiles = await tileRepository.readAll();
+  res.json(tiles);
 };
 
 const validate: RequestHandler = async (req, res, next) => {
